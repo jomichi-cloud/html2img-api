c68ca0a17ddb48a06e48e1345ee4981ccf191748
jomichi-cloud <j.omichi@buysell-technologies.com>
2025-11-05 18:07:00 +0900

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const chromium = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer-core');
 const app = express();
 app.use(express.json({limit: '2mb'}));
 
 // ★★★ 神とあなただけの合言葉を設定 ★★★
-const API_KEY = 'kami-no-aikotoba-12345'; // ここにあなただけの秘密の言葉を設定
+const API_KEY = 'kami-no-aikotoba-12345';
 
 app.post('/html2img', async (req, res) => {
   // ★★★ 合言葉をチェック ★★★
@@ -14,16 +15,30 @@ app.post('/html2img', async (req, res) => {
   }
 
   const html = req.body.html || '<h1>NO HTML</h1>';
+  let browser = null;
+
   try {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    // ★★★ 雲の上専用の魔法の筆を召喚 ★★★
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
     await page.setContent(html, {waitUntil: 'networkidle0'});
     const image = await page.screenshot({type: 'png'});
-    await browser.close();
+    
     res.set('Content-Type', 'image/png');
     res.send(image);
+
   } catch (e) {
     res.status(500).json({error: e.message});
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
   }
 });
 
